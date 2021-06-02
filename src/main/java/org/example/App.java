592bbf78763b2d231c2200ce94555da243f9aa4d
ryanpersad <Rpersad0305@gmail.com>
2021-06-02 19:22:56 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,39 @@
+package org.example;
+import java.util.Scanner;
+/**
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("How many people? ");
+        String people = myObj.nextLine();
+
+        System.out.print("How many pizzas do you have? ");
+        String pizza = myObj.nextLine();
+
+        System.out.print("How many slices per pizza? ");
+        String slices = myObj.nextLine();
+
+        //Convert to int
+        int newPpl = Integer.parseInt(people);
+        int newPizza = Integer.parseInt(pizza);
+        int newSlices = Integer.parseInt(slices);
+
+        //Arithmetic
+        int totalSlices = newSlices * newPizza;
+        int PerPerson = totalSlices/newPpl;
+        int remaining = totalSlices % newPpl;
+
+        //Output
+        String print1 = String.format("%s people with %s pizzas (%d slices)",people,pizza,totalSlices);
+        String print2 = String.format("Each person gets %d pieces of pizza.\n" +
+                "There are %d leftover pieces.",PerPerson,remaining);
+
+
+        System.out.println(print1);
+        System.out.println(print2);
+    }
+}
